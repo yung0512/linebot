761f422bf434ddf4c86325c46265d45144ca22cf
yung0512 <jeff8651215@gmail.com>
2019-12-11 11:18:40 +0800

    add  weather

--- fsm.py
+++ fsm.py
@@ -19,8 +19,10 @@ def getweather(id):
     result=''
 
     url='http://twweatherapi.appspot.com/forecast?location='+id+'&output=json'
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
 
-    req=urllib.request.urlopen(url)
+    first = urllib.request.Request(url, headers = headers)
+    req=urllib.request.urlopen(first)
 
     chiadict=eval(req.read())
 
